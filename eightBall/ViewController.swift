7f21e7002ecea32035498e15a7079081678f97d5
SirRobLyon <sirroblyon@gmail.com>
2019-09-22 21:00:32 +0100

    Initial commit

--- eightBall/ViewController.swift
+++ eightBall/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.becomeFirstResponder()
+        
+        }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            
+            let randomInt = Int.random(in: 1...20)
+            print(randomInt)
+            let sound = Bundle.main.path(forResource: String(randomInt), ofType: "mp3")
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+                
+            } catch {
+                print(error)
+            }
+            
+            audioPlayer.play()
     }
 
-
+}
 }
 
